f370ad5fce877739733efe83c17b940777d45ef7
azizimaulana <sishiro59@gmail.com>
2025-04-29 17:12:18 +0800

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,12 @@
+print ("azizi maulana al fauzali 24241141")
+inputuser = int(input("masukkan bilangan antara 24 atau lebih dari 41"))
+
+#+++++24------------
+#memeriksa angka yang kurang dari 41
+iskurangdari = (inputuser <24)
+
+#memeriksa angka yang kurang dari 41
+islebihdari = (inputuser <41)
+
+final = iskurangdari or islebihdari
+print("angka yang anda masukan", final)
