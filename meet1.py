3a0322b34a3761b408f6e61954b10e03078996d3
azizimaulana <sishiro59@gmail.com>
2025-06-26 15:28:22 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,35 @@
+# LOGIKA BOOLEANAdd commentMore actions
+ # NOT AND OR XOR
+ 
+ print("==========NOT==========")
+ x = False  # x adalah nilai boolean awal
+ z = not x  # z adalah hasil operasi NOT pada x
+ print("nilai z =", z)
+ 
+ # AND
+ print("==========AND==========")
+ x = False  # x adalah operand pertama
+ y = False  # y adalah operand kedua
+ nilai = x and y  # nilai adalah hasil AND antara x dan y
+ print(x, "and", y, "=", nilai)
+ 
+ x = False
+ y = True
+ nilai = x and y
+ print(x, "and", y, "=", nilai)
+ 
+ x = True
+ y = False
+ nilai = x and y
+ print(x, "and", y, "=", nilai)
+ 
+ x = True
+ y = True
+ nilai = x and y
+ print(x, "and", y, "=", nilai)
+ 
+ # OR
+ print("==========OR==========")
+ x = False  # x adalah operand pertama
+ y = False  # y adalah operand kedua
+ nilai = x or y  # n…
